4ae7827569fc284af9b9bca75aab74f4a047934a
Denis Rozhkov <rojkoff.d@gmail.com>
2023-07-09 17:31:22 +0300

    change gitconfig

---
+++ config.ts
@@ -0,0 +1,17 @@
+export const conf = {
+  authFirebase: {
+    email: 'test@gmail.com',
+    password: '12345678',
+  },
+  firebase: {
+    apiKey: "AIzaSyAq-VfNd5ScvBa3Wur0Oo-YKmJDv_Kh6f0",
+    authDomain: "parser-olx-72881.firebaseapp.com",
+    databaseURL: "https://parser-olx-72881-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "parser-olx-72881",
+    storageBucket: "parser-olx-72881.appspot.com",
+    messagingSenderId: "836029613508",
+    appId: "1:836029613508:web:9c3c6fc967e6800a095091"
+  },
+  botToken: '5364402068:AAHd0Ou0xUPVxn374Ji2d51GPHEInzx_39c',
+  storagePath: `${process.cwd()}/files/`,
+};
